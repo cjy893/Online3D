9df4c28205075107b0a7fd829261a95fb08bb5e4
AnonTokyo <1406634103@qq.com>
2025-01-23 19:27:06 +0800

    added output file tracking

--- implement.go
+++ implement.go
@@ -6,15 +6,19 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"sync"
 )
+
+func LineContainsPrefix(line, prefix string) bool {
+	return len(line) >= len(prefix) && prefix == line[:len(prefix)]
+}
 
 func main() {
 	trainerPath := "C:/Users/Administrator/Online3D/3DGS/gaussian-splatting/train_video.py"
 
 	cmd := exec.Command("python3", trainerPath)
 	cmd.Env = append(os.Environ(), "PYTHONPATH=C:/Users/Administrator/Online3D/3DGS/gaussian-splatting/envs/gaussian_splatting")
-
 	err := os.Chdir(filepath.Dir(trainerPath))
 	if err != nil {
 		fmt.Println("Error changing directory:", err)
@@ -27,12 +31,6 @@ func main() {
 		return
 	}
 
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		fmt.Println("Error creating stderr pipe:", err)
-		return
-	}
-
 	if err := cmd.Start(); err != nil {
 		fmt.Println("Error starting command:", err)
 		return
@@ -40,22 +38,25 @@ func main() {
 
 	fmt.Println("Training started. Waiting for completion...")
 
+	var indexPath string
 	var wg sync.WaitGroup
-	wg.Add(2)
+	wg.Add(1)
 	done := make(chan bool)
 	go func() {
 		defer wg.Done()
+		prefix := "Output folder:"
 		stdOutScanner := bufio.NewScanner(stdout)
 		for stdOutScanner.Scan() {
+			if LineContainsPrefix(stdOutScanner.Text(), prefix) {
+				line := stdOutScanner.Text()
+				first := strings.Index(line, "./output/")
+				if first == -1 {
+					fmt.Println("Output folder find error")
+					return
+				}
+				indexPath = line[first : first+19]
+			}
 			fmt.Println(stdOutScanner.Text())
-		}
-	}()
-
-	go func() {
-		defer wg.Done()
-		stdErrScanner := bufio.NewScanner(stderr)
-		for stdErrScanner.Scan() {
-			fmt.Println(stdErrScanner.Text())
 		}
 		done <- true
 	}()
@@ -77,15 +78,16 @@ func main() {
 		fmt.Println("Training finished without expected output")
 	}
 
-	viewerPath := "C:/Users/Administrator/Online3D/3DGS/gaussian-splatting/SIBR_viewer.py"
-
-	err = os.Chdir(filepath.Dir(viewerPath))
+	indexPathAbs, err := filepath.Abs(indexPath)
 	if err != nil {
-		fmt.Println("Error changing directory:", err)
+		fmt.Println("Error to convert path:", err)
 		return
 	}
 
-	cmd = exec.Command("python3", filepath.Base(viewerPath))
+	viewerPath := "C:/Users/Administrator/Online3D/3DGS/gaussian-splatting/SIBR_viewer.py"
+	err = os.Chdir(filepath.Dir(viewerPath))
+
+	cmd = exec.Command("python3", filepath.Base(viewerPath), indexPathAbs)
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		fmt.Println("Error:", err)

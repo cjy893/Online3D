45c3b76722bdffcba0320d16bd3f1edc39ac0b21
AnonTokyo <1406634103@qq.com>
2025-02-14 22:54:29 +0800

    able to process the video uploaded

--- models/video.go
+++ models/video.go
@@ -4,10 +4,11 @@ import "gorm.io/gorm"
 
 type Video struct {
 	gorm.Model
-	VideoID  string
-	UserName string
-	FileName string
-	FilePath string
-	Status   string
-	ErrorLog string
+	VideoID     string
+	UserName    string
+	FileName    string
+	FilePath    string
+	Status      string
+	ErrorLog    string
+	ProcessTime string
 }

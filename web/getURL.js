c7dd1b08369f2fed67391731ea676d0b4d3fdd2a
Lin-cmy <1127934124@qq.com>
2025-03-15 14:43:31 +0800

    Create getURL.js

---
+++ web/getURL.js
@@ -0,0 +1,20 @@
+// Function to get query parameters from URL
+function getQueryParams() {
+	const params = {};
+	const queryString = window.location.search.substring(1);
+	const regex = /([^&=]+)=([^&]*)/g;
+	let matches;
+	while (matches = regex.exec(queryString)) {
+		params[decodeURIComponent(matches[1])] = decodeURIComponent(matches[2]);
+	}
+	return params;
+}
+
+// Get work_id from query parameters
+const queryParams = getQueryParams();
+const workID = queryParams['work_id'] || "{{WORK_ID}}";
+const url = new URL(`/user/GetWork/${workID}`, window.location.origin);
+
+// Make url globally available
+window.generatedUrl = url;
+console.log(url); // For debugging purposes

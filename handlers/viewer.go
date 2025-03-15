5f0fee1d0170c549c11139dcb057cf3afaddc10b
Lin-cmy <1127934124@qq.com>
2025-03-15 14:14:54 +0800

    Update viewer.go

--- handlers/viewer.go
+++ handlers/viewer.go
@@ -1,16 +1,9 @@
 package handlers
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 func SplatViewer(c *gin.Context) {
-	workID := c.Query("work_id")
-	if workID == "" {
-		c.String(http.StatusBadRequest, "work_id is required")
-		return
-	}
 	c.File("./web/index.html")
 }

a93624526de8f24f3133f61f5347789d762eff31
AnonTokyo <1406634103@qq.com>
2025-05-06 00:40:40 +0800

    add the function of search by title

--- handlers/work.go
+++ handlers/work.go
@@ -370,7 +370,7 @@ func SearchWorks(c *gin.Context) {
 	pageSize := 20
 
 	var works []models.Work
-	if err := config.Conf.DB.Where("work_name LIKE ? and is_public = true", "%"+q+"%").
+	if err := config.Conf.DB.Where("work_name LIKE ?", "%"+q+"%").Where("is_public = ?", true).
 		Offset((page - 1) * pageSize).
 		Limit(pageSize).
 		Find(&works).Error; err != nil {
